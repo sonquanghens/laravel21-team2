cb30797f1c28d595a849b7aff3dead993d4102fd
= <=>
2017-12-17 09:35:26 +0700

    from_admin_user

---
+++ resources/views/admin/contents/content.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layouts.admin_master')
+@section('content')
+  <div id="page-inner">
+      <div class="row">
+          <div class="col-md-12">
+              <h1 class="page-head-line">DASHBOARD</h1>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-12">
+              <div class="alert alert-info">
+                Hello
+              </div>
+          </div>
+      </div>
+
+  <!-- /. PAGE INNER  -->
+  </div>
+@stop
---
+++ resources/views/user/layouts/header.blade.php
@@ -0,0 +1,30 @@
+<div class="header-top">
+  <div class="container">
+    <div class="pull-right auto-width-right">
+      <ul class="top-details menu-beta l-inline">
+        <li><a href="#"><i class="fa fa-user"></i>Tài khoản</a></li>
+        <li><a href="#">Đăng kí</a></li>
+        <li><a href="#">Đăng nhập</a></li>
+      </ul>
+    </div>
+    <div class="clearfix"></div>
+  </div> <!-- .container -->
+</div> <!-- .header-top -->
+<div class="header-body">
+  <div class="container beta-relative">
+    <div class="pull-left">
+      <a href="index.html" id="logo"><img src="logo-fandy-noel.png" width="200px" alt=""></a>
+    </div>
+    <div class="pull-right beta-components space-left ov">
+      <div class="space10">&nbsp;</div>
+      <div class="beta-comp">
+        <form role="search" method="get" id="searchform" action="/">
+              <input type="text" value="" name="s" id="s" placeholder="Nhập từ khóa..." />
+              <button  type="submit" id="searchsubmit" class="fa fa-search"></button>
+        </form>
+      </div>
+
+    </div>
+    <div class="clearfix"></div>
+  </div> <!-- .container -->
+</div> <!-- .header-body -->
